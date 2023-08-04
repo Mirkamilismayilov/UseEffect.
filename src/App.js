2f92818a345bda45acd3dc9e9490b1c27d150504
Mirkamil İsmayılov <118895408+Mirkamilismayilov@users.noreply.github.com>
2023-08-04 11:01:42 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import "./index.css"
+import Effect from "./components/Effect";
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+    <Effect/>
+
+    </div>
+  );
+}
+
+export default App;
